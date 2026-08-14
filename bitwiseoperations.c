6be602c808438e8b4bc70bec073b871b17b63ec0
joetedt <joeted390@gmail.com>
2026-08-15 00:21:27 +0300

    Initial commit

---
+++ bitwiseoperations.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+void calculate_the_maximum(int n, int k);//function
+
+int main(){
+    int n,k;
+    
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n,k);
+
+    return 0;
+}
+void calculate_the_maximum( int n, int k){ //finds the maximum values of bitwise comparison operators and or and Xor
+    int a=1;
+    int b= a +1;//a and b show possible combinations of numbers
+    int max_and, max_or,max_xor;
+    max_and=max_or=max_xor=0;
+    for (a = 1; a<n; a++){
+        for (b =a +1; b<=n;b++){
+            if ((a&b)< k){
+                int w =(a&b);
+                if (w> max_and){
+                    max_and=w;   
+                }
+            }
+        }
+    }
+    for (a = 1; a<n; a++){
+        for (b =a +1; b<=n;b++){
+            if ((a|b)< k){
+                int m =(a|b);
+                if (m> max_or){
+                    max_or=m;   
+                }
+            }
+        }
+    }
+    for (a = 1; a<n; a++){
+        for (b =a +1; b<=n;b++){
+            if ((a^b)< k){
+                int p =(a^b);
+                if (p > max_xor){
+                    max_xor=p; 
+                }
+            }
+        }
+    }
+    printf("%d\n", max_and);
+    printf("%d\n", max_or);
+    printf("%d\n",max_xor);
+}
---
+++ numtoSum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int convertNumtoarray(int a);//converts number into array
+
+int num, i, num_array[5], sum;
+
+int main(){
+    scanf("%d", &num);
+    convertNumtoarray(num);
+    printf("%d\n", sum);
+
+    return 0;
+}
+
+int convertNumtoarray(int a){
+    if(a >= 10000 && a<= 99999){
+        sum = 0;
+        for (i=0; i<5; i++){
+            num_array[i] = num %10;
+            num = num/10;
+            printf("%d\n", num_array[i]); //if u need the list of numbers
+            sum += num_array[i];
+    
+        }
+
+        // printf("%d\n", sum);
+    }
+    return sum;
+}
