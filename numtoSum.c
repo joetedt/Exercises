e4d1b83b46fe9bbd137fe8437319507946e81f86
joetedt <joeted390@gmail.com>
2026-09-04 21:13:47 +0300

    added more details

--- numtoSum.c
+++ numtoSum.c
@@ -4,23 +4,24 @@ int convertNumtoarray(int a);//converts number into array
 
 int num, i, num_array[5], sum;
 int main(){
+    printf("Enter Number:");
     scanf("%d", &num);
     convertNumtoarray(num);
-    printf("%d\n", sum);
-
+    printf("The sum is:%d\n", sum);
     return 0;
 }
 int convertNumtoarray(int a){
     if(a >= 10000 && a<= 99999){
         sum = 0;
         for (i=0; i<5; i++){
-            num_array[i] = num %10;
+            num_array[i] = num %10; //draws out the last digit in the number after every run
             num = num/10;
-            printf("%d\n", num_array[i]); //if u need the list of numbers
+            printf("%d\n", num_array[i]); //if u need the list of how numbers appear
             sum += num_array[i];
     
         }
         // printf("%d\n", sum);
+
     }
     return sum;// return sum as final value to be used else where in the expression, void would work too
     //i think
--- recursivefunctions.c
+++ recursivefunctions.c
@@ -1,11 +1,17 @@
 //finding nth number in a series where the first 3 numbers are given
+//the nth term is found as it is the sum of the three previous numbers
+//so for example given the first three numbers as 1, 2, 3 the fourth number
+//is 6 (1 + 2 + 3 = 6)
 #include <stdio.h>
 int a,b,c;
 int series(int n);
 int main(){
     int n;
-    scanf("%d %d %d %d", &n, &a, &b, &c);
-    printf("%d\n", series(n));
+    printf("Enter the position n of number required:");
+    scanf("%d", &n);
+    printf("Enter the first 3 numbers in the series:\n");
+    scanf("%d %d %d", &a, &b, &c);
+    printf("The position %d number is: %d\n", n, series(n));
     return 0;
 }
 
